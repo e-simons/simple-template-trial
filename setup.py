939295bcfd0d741a15689a48f436c0c2382bc643
e-simons <93005336+e-simons@users.noreply.github.com>
2025-01-24 12:43:28 +0000

    ✅ Ready to clone and code.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for simple_template_trial package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("simple_template_trial", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="simple_template_trial",
+    version=read("simple_template_trial", "VERSION"),
+    description="Awesome simple_template_trial created by e-simons",
+    url="https://github.com/e-simons/simple-template-trial/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="e-simons",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["simple_template_trial = simple_template_trial.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
---
+++ simple_template_trial/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for simple_template_trial."""
+
+from simple_template_trial.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
